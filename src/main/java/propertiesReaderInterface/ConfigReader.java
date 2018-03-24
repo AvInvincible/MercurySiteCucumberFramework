994e0543a8efad70b26788e223dc8cf184a8447a
Ajit05 <ajity050@gmail.com>
2018-03-24 23:04:10 +0530

    added_conditions_to_handle_gradle_taskAndOffLineExecution

--- src/main/java/propertiesReaderInterface/ConfigReader.java
+++ src/main/java/propertiesReaderInterface/ConfigReader.java
@@ -5,5 +5,6 @@ import browser.SupportedBrowser;
 /* Interface for all the key-value pair defined in .properties files */
 public interface ConfigReader {
 	public String readData(String x);
+	public String readConfigData(String x);
 	public SupportedBrowser getBrowser(); 
 }
